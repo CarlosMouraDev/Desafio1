0c870802a69635755c747cd28db41650eee5da51
Carlos Moura <carlos@email.com>
2025-01-24 17:45:44 -0300

    Transaction log implementation

--- src/main/java/br/com/compass/repositories/TransactionRepository.java
+++ src/main/java/br/com/compass/repositories/TransactionRepository.java
@@ -1,7 +1,11 @@
 package br.com.compass.repositories;
 
+import br.com.compass.entities.Account;
 import br.com.compass.entities.Transaction;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class TransactionRepository {
 
@@ -16,4 +20,13 @@ public class TransactionRepository {
         em.persist(transaction);
         em.getTransaction().commit();
     }
+
+    public List<Transaction> findTransactionsByAccount(Account account) {
+        TypedQuery<Transaction> query = em.createQuery(
+                "SELECT t FROM Transaction t WHERE t.originAccount = :account OR t.destinationAccount = :account",
+                Transaction.class);
+        query.setParameter("account", account);
+        return query.getResultList();
+    }
+
 }
--- src/main/java/br/com/compass/services/BankService.java
+++ src/main/java/br/com/compass/services/BankService.java
@@ -7,6 +7,9 @@ import br.com.compass.repositories.AccountRepository;
 import br.com.compass.repositories.TransactionRepository;
 import br.com.compass.services.exceptions.BankServiceException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BankService {
     private final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
@@ -40,10 +43,59 @@ public class BankService {
 
         Transaction transaction = new Transaction();
         transaction.setOriginAccount(account);
-        transaction.setTransactionType(TransactionType.WITHDRAWAL);
+        transaction.setTransactionType(TransactionType.WITHDRAW);
         transaction.setValue(amount);
         transaction.setTransactionDate(java.time.LocalDate.now());
         transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> getTransactionHistory(Account loggedUser) {
+        List<Transaction> allTransactions = transactionRepository.findTransactionsByAccount(loggedUser);
+        return allTransactions.stream()
+                .filter(transaction ->
+                        (transaction.getOriginAccount().equals(loggedUser) &&
+                                (transaction.getTransactionType() == TransactionType.DEPOSIT ||
+                                        transaction.getTransactionType() == TransactionType.WITHDRAW ||
+                                        transaction.getTransactionType() == TransactionType.SENT_TRANSFER)) ||
+                                (transaction.getDestinationAccount().equals(loggedUser) &&
+                                        transaction.getTransactionType() == TransactionType.RECEIVED_TRANSFER)
+                )
+                .collect(Collectors.toList());
+    }
+
+    public void transfer(String recipient, String receiver, double amount) throws BankServiceException {
+        Account account1 = accountRepository.findByEmail(recipient);
+        Account account2 = accountRepository.findByEmail(receiver);
+
+        if (account2 == null) {
+            throw new BankServiceException("Account not found.");
+        }
+
+        if (account1.getBalance() < amount) {
+            throw new BankServiceException("Insufficient funds.");
+        }
+        
+        account1.decreaseBalance(amount);
+        account2.increaseBalance(amount);
+
+        Transaction transactionSent = new Transaction();
+        transactionSent.setOriginAccount(account1);
+        transactionSent.setDestinationAccount(account2);
+        transactionSent.setTransactionType(TransactionType.SENT_TRANSFER);
+        transactionSent.setValue(amount);
+        transactionSent.setTransactionDate(java.time.LocalDate.now());
+        transactionRepository.save(transactionSent);
+
+        Transaction transactionReceived = new Transaction();
+        transactionReceived.setOriginAccount(account1);
+        transactionReceived.setDestinationAccount(account2);
+        transactionReceived.setTransactionType(TransactionType.RECEIVED_TRANSFER);
+        transactionReceived.setValue(amount);
+        transactionReceived.setTransactionDate(java.time.LocalDate.now());
+        transactionRepository.save(transactionReceived);
+
+        accountRepository.update(account1);
+        accountRepository.update(account2);
     }
 
 }
